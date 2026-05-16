496c6f7df60f420de10c83367eca9ba51eb83af3
elenco.calado <elencio.dev.studio@gmail.com>
2026-05-16 09:19:37 -0300

    feat: adicionar autenticação para rota; adicionar validação de tipos dos arquivos

--- Http/main.go
+++ Http/main.go
@@ -6,7 +6,19 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
+
+func validarTipo(contentType string) bool {
+	tiposPermitidos := map[string]bool{
+		"image/jpeg":      true,
+		"image/png":       true,
+		"application/pdf": true,
+	}
+
+	permitido, existe := tiposPermitidos[contentType]
+	return existe && permitido
+}
 
 func HandlerUpload(w http.ResponseWriter, r *http.Request) {
 
@@ -15,6 +27,7 @@ func HandlerUpload(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Metodo não permitido", http.StatusMethodNotAllowed)
 		return
 	}
+
 	r.Body = http.MaxBytesReader(w, r.Body, 10<<20) //limitar o tamnho do upload do arquivo
 	err := r.ParseMultipartForm(10 << 20)
 
@@ -31,7 +44,26 @@ func HandlerUpload(w http.ResponseWriter, r *http.Request) {
 
 	defer file.Close()
 
-	dst, err := os.Create("./uploads/" + header.Filename)
+	// le os primeiros bytes para verificar o tipo real do arquivo
+	buffer := make([]byte, 512)
+	_, err = file.Read(buffer)
+	if err != nil {
+		http.Error(w, "Erro ao ler arquivo: ", http.StatusInternalServerError)
+		return
+	}
+
+	contentType := http.DetectContentType(buffer)
+
+	if !validarTipo(contentType) {
+		http.Error(w, "Tipo de arquivo não Permitido"+contentType, http.StatusBadRequest)
+		return
+	}
+
+	file.Seek(0, io.SeekStart)
+
+	name := filepath.Base(header.Filename)
+
+	dst, err := os.Create("./uploads/" + name)
 	if err != nil {
 		http.Error(w, "Erro ao salvar arquivo: ", http.StatusInternalServerError)
 		return
@@ -48,6 +80,24 @@ func HandlerUpload(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func basicAuth(handler http.HandlerFunc, usuarios map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user, pass, ok := r.BasicAuth()
+		if !ok {
+			w.Header().Set("WWW-Authenticate", `Basic realm="Restrito"`)
+			http.Error(w, "Autenticação necessaria", http.StatusUnauthorized)
+			return
+		}
+
+		if senha, existe := usuarios[user]; !existe || senha != pass {
+			http.Error(w, "Credencias Inválidas", http.StatusUnauthorized)
+			return
+		}
+
+		handler(w, r)
+	}
+}
+
 func main() {
 
 	os.MkdirAll("./uploads", 0755)
@@ -55,9 +105,14 @@ func main() {
 	mux := http.NewServeMux() //multiplexador de rotas
 	fs := http.FileServer(http.Dir(dir))
 
+	usuarios := map[string]string{ //criando usuarios
+		"admin": "123456",
+		"user":  "senha",
+	}
+
 	//rotas
 	mux.Handle("/", fs)
-	mux.HandleFunc("/upload", HandlerUpload)
+	mux.HandleFunc("/upload", basicAuth(HandlerUpload, usuarios))
 
 	log.Println("Servindo" + dir + "em http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
